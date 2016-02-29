aa7de8a289e0fbdf7077f96b92385ee4c92926f9
yft <yftang@freewheel.tv>
2016-02-29 20:29:22 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ SampleApp::Application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
